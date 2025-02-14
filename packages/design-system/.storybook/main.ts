5002c45d9822148e18467e8a9240571cecfa5e9b
Jeong-Ag <kjungu1105@gmail.com>
2025-02-14 23:28:19 +0900

    chore(package): set design-system

---
+++ packages/design-system/.storybook/main.ts
@@ -0,0 +1,26 @@
+import type { StorybookConfig } from '@storybook/react-vite';
+import { dirname, join } from 'path';
+
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, 'package.json')));
+}
+const config: StorybookConfig = {
+  stories: [
+    '../src/**/*.mdx',
+    '../src/stories/**/*.mdx',
+    '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+  ],
+  addons: [
+    getAbsolutePath('@storybook/addon-onboarding'),
+    getAbsolutePath('@storybook/addon-links'),
+    getAbsolutePath('@storybook/addon-essentials'),
+    getAbsolutePath('@chromatic-com/storybook'),
+    getAbsolutePath('@storybook/addon-interactions'),
+    getAbsolutePath('storybook-addon-pseudo-states'),
+  ],
+  framework: getAbsolutePath('@storybook/react-vite'),
+  docs: {
+    autodocs: 'tag',
+  },
+};
+export default config;
---
+++ packages/design-system/tailwind.config.js
@@ -0,0 +1,30 @@
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        dark: 'rgb(61, 56, 47)',
+        light: 'rgb(159, 156, 149)',
+        point: 'rgb(255, 166, 0)',
+        sub: 'rgb(255, 255, 255)',
+        'point-dark': 'rgb(241, 149, 0)',
+        'sub-dark': 'rgb(255, 245, 225)',
+        normal: 'rgb(255, 255, 255)',
+        'normal-dark': 'rgb(248, 248, 248)',
+        lock: 'rgb(245, 244, 241)',
+        toggle: 'rgb(234, 230, 220)',
+      },
+      fontSize: {
+        sm: '14px',
+        md: '16px',
+        lg: '20px',
+        xl: '24px',
+      },
+      boxShadow: {
+        inner: 'inset 1.5px 1.5px 1.5px 0 rgba(0, 0, 0, 0.08)',
+      },
+    },
+  },
+  plugins: [],
+  important: true,
+};
