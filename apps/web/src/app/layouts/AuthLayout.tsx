cf6039cff9c4caf3d6c3a79e9d2d90adace456f1
cho4u4o <cho4u4o@gmail.com>
2025-02-19 02:41:28 +0900

    feat(web): implement auth page layout

---
+++ apps/web/src/app/layouts/AuthLayout.tsx
@@ -0,0 +1,18 @@
+import AuthPageImg from '~/assets/images/login-signup-image.png';
+import { Outlet } from 'react-router-dom';
+
+export default function AuthLayout() {
+  return (
+    <div className="grid min-h-screen grid-cols-1 xl:grid-cols-2">
+      <div className="flex size-full justify-center py-20">
+        <div className="border-dash-board-border w-170 rounded-auth box-shadow box-border h-full border-[1px] p-16">
+          <Outlet />
+        </div>
+      </div>
+      <div
+        className="hidden size-full bg-cover bg-center xl:block"
+        style={{ backgroundImage: `url(${AuthPageImg})` }}
+      ></div>
+    </div>
+  );
+}
