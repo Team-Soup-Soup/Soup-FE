edf55b4891cf8e988e26b6d6b6bac923d3724004
cho4u4o <cho4u4o@gmail.com>
2025-02-19 00:10:03 +0900

    design(land): add comma

--- apps/land/src/widgets/home/ui/HomeContainer.tsx
+++ apps/land/src/widgets/home/ui/HomeContainer.tsx
@@ -39,9 +39,23 @@ function Header() {
   return (
     <div className="relative flex h-[505px] w-full bg-cover bg-no-repeat">
       <div className="absolute inset-0 z-20 box-border flex h-[505px] w-[896px] justify-start pl-[80px] pr-[110px] font-medium text-white md:justify-end">
-        <div className="mb-[55px] mt-[137px] flex flex-col place-items-start gap-y-4">
-          <div>{parse(TEXT.HEADER.TITLE, ['text-xl ', 'text-2xl '])}</div>
+        <div className="relative mb-[55px] mt-[137px] flex flex-col place-items-start gap-y-4">
+          <Image
+            src="/images/land-header-comma-down.png"
+            alt="circle"
+            width={30}
+            height={50}
+            className="absolute left-[-44px]"
+          />
+          <div>{parse(TEXT.HEADER.TITLE, ['text-xl', 'text-2xl'])}</div>
           <div className="text-lg">{parse(TEXT.HEADER.SUBTITLE)}</div>
+          <Image
+            src="/images/land-header-comma-up.png"
+            alt="circle"
+            width={30}
+            height={50}
+            className="absolute right-[-44px] top-11"
+          />
           <div className="flex w-full justify-end">
             <button className="border-text-dark rounded-4xl mt-[66px] h-fit w-fit cursor-pointer border-[1px] bg-black/20 px-9 py-3 text-xl focus:outline-none">
               바로 시작하기
