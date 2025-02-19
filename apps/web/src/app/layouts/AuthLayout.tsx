ee8fc5f142f9a2f0d72f4beaa5087cb9f55f3d07
cho4u4o <cho4u4o@gmail.com>
2025-02-20 01:23:29 +0900

    fix(web): solve ci error by annotating type

--- apps/web/src/app/layouts/AuthLayout.tsx
+++ apps/web/src/app/layouts/AuthLayout.tsx
@@ -1,7 +1,7 @@
 import AuthPageImg from '~/assets/images/login-signup-image.png';
 import { Outlet } from 'react-router-dom';
 
-export default function AuthLayout() {
+export default function AuthLayout(): React.ReactElement {
   return (
     <div className="grid min-h-screen grid-cols-1 xl:grid-cols-2">
       <div className="flex size-full justify-center py-20">
