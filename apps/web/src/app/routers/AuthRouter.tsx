e83761eaf08062a8b91e7983357c5fefc13f9c1a
cho4u4o <cho4u4o@gmail.com>
2025-02-20 12:37:41 +0900

    remove(web): delete `index.ts` of `shared` directory

--- apps/web/src/app/routers/AuthRouter.tsx
+++ apps/web/src/app/routers/AuthRouter.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
-import { useLoginStatus } from '~/shared';
+import { useLoginStatus } from '~/shared/hooks';
 
 export default function AuthRouter() {
   return useLoginStatus() ? <Outlet /> : <Navigate to="/login" />;
