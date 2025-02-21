2cb1985831479f2d3f8adc7ca0066c30829759b4
cho4u4o <cho4u4o@gmail.com>
2025-02-21 17:12:58 +0900

    design(design-system): change `Button` border color

--- packages/design-system/src/Button/Button.styles.ts
+++ packages/design-system/src/Button/Button.styles.ts
@@ -8,11 +8,12 @@ export const buttonVariants = cva(
         point:
           'bg-point text-white border-point hover:bg-point-dark font-semibold',
         sub: 'bg-sub text-point border-point hover:bg-sub-dark',
-        normal: 'bg-normal hover:bg-normal-dark border-button text-dark',
+        normal:
+          'bg-normal hover:bg-normal-dark border-main-board-border text-dark',
       },
       size: {
         lg: 'py-[12px] px-[28px]',
-        md: 'py-[8px] px-[20px]',
+        md: 'py-[8px] px-[20px] ',
         sm: 'px-[12px] text-sm',
       },
       locked: {
