e05285092e6d1f1c4e1df0b67d164c444aa29bac
cho4u4o <cho4u4o@gmail.com>
2025-02-21 17:16:42 +0900

    feat(web): add signup page to route

--- apps/web/src/app/routes/AuthRoutes.tsx
+++ apps/web/src/app/routes/AuthRoutes.tsx
@@ -1,13 +1,15 @@
 import { type RouteObject } from 'react-router-dom';
 
-import { LoginPage } from '~/pages/login/ui';
 import { AuthLayout } from '~/app/layouts';
 import { PATH } from '~/shared/constants';
+
+import { LoginPage } from '~/pages/login/ui';
+import { SignupPage } from '~/pages/signup/ui';
 
 export const AuthRoutes: RouteObject = {
   element: <AuthLayout />,
   children: [
     { path: PATH.LOGIN, element: <LoginPage /> },
-    { path: PATH.SIGNUP },
+    { path: PATH.SIGNUP, element: <SignupPage /> },
   ],
 };
