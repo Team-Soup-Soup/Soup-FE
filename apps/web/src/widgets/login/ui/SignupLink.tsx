9f415dc5393f1860d9ba6d27686e9f2815649f4f
cho4u4o <cho4u4o@gmail.com>
2025-02-21 17:17:27 +0900

    design(web): change hover color

--- apps/web/src/widgets/login/ui/SignupLink.tsx
+++ apps/web/src/widgets/login/ui/SignupLink.tsx
@@ -5,7 +5,7 @@ export default function SignupLink() {
   return (
     <div className="text-light absolute bottom-0 left-0 w-fit font-light">
       <span className="mr-8">계정이 없으신가요?</span>
-      <Link to="/signup" className="hover:text-dark cursor-pointer">
+      <Link to="/signup" className="hover:text-point cursor-pointer">
         <u>회원가입하기</u>
       </Link>
     </div>
