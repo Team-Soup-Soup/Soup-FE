9c8c8cc79e84d3f55b403009ee07cb751c8d1fbf
cho4u4o <cho4u4o@gmail.com>
2025-02-21 20:39:50 +0900

    refactor(web): fix `Link`

--- apps/web/src/features/login/ui/LoginOption.tsx
+++ apps/web/src/features/login/ui/LoginOption.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import { Checkbox } from '@soup/design-system';
 import { cn } from '@soup/utils';
+import { Link } from 'react-router-dom';
 
 export default function LoginOption({ className }: { className?: string }) {
   return (
     <div className={cn('flex w-full justify-between', className)}>
       <Checkbox id="rememberLogin" label="로그인 상태 유지" />
       <div className="flex gap-x-11">
-        <a className="text-light hover:text-dark cursor-pointer font-light">
+        <Link
+          to=""
+          className="text-light hover:text-dark cursor-pointer font-light"
+        >
           아이디 찾기
-        </a>
-        <a className="text-light hover:text-dark cursor-pointer font-light">
+        </Link>
+        <Link
+          to=""
+          className="text-light hover:text-dark cursor-pointer font-light"
+        >
           비밀번호 찾기
-        </a>
+        </Link>
       </div>
     </div>
   );
