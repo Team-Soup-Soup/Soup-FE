2407b4330473ed0542063e72bdfa01360f798fa6
cho4u4o <cho4u4o@gmail.com>
2025-02-21 14:46:06 +0900

    design(design-system): change input text color

--- packages/design-system/src/Input/Input.tsx
+++ packages/design-system/src/Input/Input.tsx
@@ -38,7 +38,7 @@ const Input = ({
           id={id}
           ref={inputRef}
           className={cn(
-            'text-light border-main-board-border focus:border-point size-full rounded-[10px] border p-[10px] font-light focus:outline-none',
+            'border-main-board-border focus:border-point size-full rounded-[10px] border p-[10px] font-light focus:outline-none',
             isSearch && 'pl-[44px]',
             inputClassName,
           )}
