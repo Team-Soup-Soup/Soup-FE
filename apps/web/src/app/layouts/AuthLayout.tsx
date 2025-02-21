06612c6c23aa0c90dc2704a2ef9bad1e6849a38d
cho4u4o <cho4u4o@gmail.com>
2025-02-21 17:14:55 +0900

    refactor(web): move padding into child

--- apps/web/src/app/layouts/AuthLayout.tsx
+++ apps/web/src/app/layouts/AuthLayout.tsx
@@ -5,7 +5,7 @@ export default function AuthLayout(): React.ReactElement {
   return (
     <div className="grid min-h-screen grid-cols-1 xl:grid-cols-2">
       <div className="flex size-full justify-center py-20">
-        <div className="border-dash-board-border w-170 rounded-auth box-shadow box-border h-full border-[1px] p-16">
+        <div className="border-dash-board-border w-170 rounded-auth box-shadow box-border h-full border-[1px]">
           <Outlet />
         </div>
       </div>
--- apps/web/src/pages/login/ui/LoginPage.tsx
+++ apps/web/src/pages/login/ui/LoginPage.tsx
@@ -3,7 +3,7 @@ import { LoginContainer } from '~/widgets/login/ui';
 
 export default function LoginPage() {
   return (
-    <div className="flex size-full flex-col">
+    <div className="flex size-full flex-col p-16">
       <p className="logo mb-8 text-center">soup</p>
       <LoginContainer />
     </div>
