8aeb11904521ae875598dc1f121463c8858b120c
cho4u4o <cho4u4o@gmail.com>
2025-02-26 23:26:14 +0900

    refactor(web): refactor `useSubmit.ts`

--- apps/web/src/features/login/model/hooks/useSubmit.ts
+++ apps/web/src/features/login/model/hooks/useSubmit.ts
@@ -1,7 +1,7 @@
-import { UserInfo } from '~/features/login/types';
+import type { UserInfo } from '~/features/login/types';
 
-export const useSubmit = (data: UserInfo) => {
+export const useSubmit = ({ username, password }: UserInfo) => {
   alert(
-    `id: ${data.username}\npw: ${data.password}\n\n로그인 요청되었습니다\n세부 구현 로직 차후 구현`,
+    `id: ${username}\npw: ${password}\n\n로그인 요청되었습니다\n세부 구현 로직 차후 구현`,
   );
 };
