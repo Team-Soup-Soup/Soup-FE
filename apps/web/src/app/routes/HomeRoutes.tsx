1c6ba9e303b0b1bcb638d214346997f2bd0edcd7
cho4u4o <cho4u4o@gmail.com>
2025-02-28 16:03:14 +0900

    feat(web): add route of `ProjectPage`

--- apps/web/src/app/routes/HomeRoutes.tsx
+++ apps/web/src/app/routes/HomeRoutes.tsx
@@ -2,9 +2,13 @@ import { type RouteObject } from 'react-router-dom';
 
 import { HomeLayout } from '~/app/layouts';
 import { HomePage } from '~/pages/home/ui';
+import { ProjectPage } from '~/pages/project/ui';
 import { PATH } from '~/shared/constants';
 
 export const HomeRoutes: RouteObject = {
   element: <HomeLayout />,
-  children: [{ path: PATH.HOME, element: <HomePage /> }],
+  children: [
+    { path: PATH.HOME, element: <HomePage /> },
+    { path: PATH.PROJECT + ':projectId', element: <ProjectPage /> },
+  ],
 };
