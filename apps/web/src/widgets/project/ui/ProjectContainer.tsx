e114525c9bdab9b1c7f2bf314dfc7083c803abbd
cho4u4o <cho4u4o@gmail.com>
2025-02-28 16:13:26 +0900

    feat(web): implement project header

---
+++ apps/web/src/widgets/project/ui/ProjectContainer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { ProjectHeader, ProjectMenu } from '~/widgets/project/ui';
+
+export default function ProjectContainer() {
+  return (
+    <div>
+      <ProjectHeader />
+      <ProjectMenu />
+    </div>
+  );
+}
