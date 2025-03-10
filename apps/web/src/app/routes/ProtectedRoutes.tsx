3e31b21f78cc573e7cbb8bcb36c90d5d17add2e8
Jeong-Ag <kjungu1105@gmail.com>
2025-03-10 17:23:53 +0900

    feat(web): apply error page on `routes`

--- apps/web/src/app/routes/ProtectedRoutes.tsx
+++ apps/web/src/app/routes/ProtectedRoutes.tsx
@@ -2,8 +2,10 @@ import { RouteObject } from 'react-router-dom';
 
 import { AuthRouter } from '~/app/routers';
 import { HomeRoutes } from '~/app/routes/HomeRoutes';
+import { ErrorPage } from '~/pages/error/ui';
 
 export const ProtectedRoutes: RouteObject = {
   element: <AuthRouter />,
+  errorElement: <ErrorPage />,
   children: [HomeRoutes],
 };
