ec8ac6711ddc69e2ab24b9603210fbc87bd82ad3
Jeong-Ag <kjungu1105@gmail.com>
2025-03-23 22:09:47 +0900

    refactor(web): remove useEffect and use const for background color

--- apps/web/src/shared/ui/LevelBadge.tsx
+++ apps/web/src/shared/ui/LevelBadge.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import type { ProfileLevel } from '../types';
 import { cn } from '@soup/utils';
 
@@ -6,34 +6,22 @@ interface LevelBadgeProps {
   level: ProfileLevel;
 }
 
+const levelStyle = {
+  master: 'bg-notice-section',
+  subMaster: 'bg-peer-review-section',
+  classic: 'bg-free-section',
+};
+
 export default function LevelBadge({ level }: LevelBadgeProps) {
-  const [backgroundColor, setBackgroundColor] = useState('');
-  const [badgeText, setBadgeText] = useState('');
-
-  useEffect(() => {
-    switch (level) {
-      case 'master':
-        setBackgroundColor('bg-notice-section');
-        break;
-      case 'subMaster':
-        setBackgroundColor('bg-peer-review-section');
-        break;
-      case 'classic':
-        setBackgroundColor('bg-free-section');
-        break;
-    }
-    setBadgeText(level[0].toUpperCase());
-  }, [level]);
-
   return (
     <>
       <div
         className={cn(
           'flex h-[16px] w-[24px] items-center justify-center text-sm font-light',
-          backgroundColor,
+          levelStyle[level],
         )}
       >
-        {badgeText}
+        {level[0].toUpperCase()}
       </div>
     </>
   );
