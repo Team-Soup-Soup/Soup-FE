38ade3243560f8d347070df73c7c108c241c25a6
cho4u4o <cho4u4o@gmail.com>
2025-03-24 22:25:53 +0900

    refactor(web): use `Profile`

--- apps/web/src/shared/ui/UserItem.tsx
+++ apps/web/src/shared/ui/UserItem.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Checkbox } from '@soup/design-system';
+import Profile from './Profile';
 
 export default function UserItem({ id }: { id: string }) {
   return (
     <div className="flex h-10 flex-shrink-0 items-center gap-x-3">
       <Checkbox id={id} checkboxClassName="border-dark size-4.5" />
-      <div className="size-8 rounded-[50%] bg-black bg-cover bg-center" />
-      <span>칠가이</span>
+      <Profile name="칠가이" imageClassName="size-9" />
     </div>
   );
 }
