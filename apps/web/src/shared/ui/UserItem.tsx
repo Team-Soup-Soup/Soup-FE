9454f91ee8ff1fb7b4775a091d507e383b49163f
cho4u4o <cho4u4o@gmail.com>
2025-03-24 15:00:00 +0900

    refactor(web): separate UserItem into shared directory

---
+++ apps/web/src/shared/ui/UserItem.tsx
@@ -0,0 +1,11 @@
+import { Checkbox } from '@soup/design-system';
+
+export default function UserItem({ id }: { id: string }) {
+  return (
+    <div className="flex h-10 flex-shrink-0 items-center gap-x-3">
+      <Checkbox id={id} checkboxClassName="border-dark size-4.5" />
+      <div className="size-8 rounded-[50%] bg-black bg-cover bg-center" />
+      <span>칠가이</span>
+    </div>
+  );
+}
