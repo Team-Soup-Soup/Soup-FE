ea77d7e01b9b15d5aa689aae3b21253f93ee693c
cho4u4o <cho4u4o@gmail.com>
2025-04-09 18:11:59 +0900

    feat(web): define `get` method with baseURL

---
+++ apps/web/src/shared/api/axios.ts
@@ -0,0 +1,28 @@
+import axios, { AxiosHeaders, AxiosResponse } from 'axios';
+
+interface GetRequestParams<TParams> {
+  request: string;
+  headers: AxiosHeaders;
+  params: TParams;
+}
+
+const instance = axios.create({
+  baseURL: 'http://student-p.p-e.kr/api',
+});
+
+export async function get<TResponse, TParams = unknown>(
+  config: GetRequestParams<TParams>,
+): Promise<AxiosResponse<TResponse>> {
+  const { request, headers, params } = config;
+  try {
+    const response = await instance.get<TResponse>(request, {
+      params: params,
+      headers: headers,
+    });
+    return response;
+  } catch (error: unknown) {
+    console.log(error);
+    if (axios.isAxiosError(error)) throw new Error(error.message);
+    else throw new Error('에러가 발생했습니다');
+  }
+}
