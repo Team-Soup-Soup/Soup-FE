01f51432faed4d7b32a14e1d887a6c219c75e9cf
cho4u4o <cho4u4o@gmail.com>
2025-04-13 19:15:31 +0900

    style(web): fix style of loading page

--- apps/web/src/pages/loading/ui/LoadingPage.tsx
+++ apps/web/src/pages/loading/ui/LoadingPage.tsx
@@ -3,7 +3,7 @@ import { Loader } from '~/assets/images';
 
 export default function LoadingPage() {
   return (
-    <div className="grid size-full place-items-center">
+    <div className="grid h-screen w-full place-items-center">
       <div className="text-point flex flex-col items-center gap-6">
         <img src={Loader} className="h-26 w-24" />
         <p className="font-semibold">스프를 끓여 오는 중...</p>
