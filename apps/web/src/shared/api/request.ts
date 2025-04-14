d8b227f2c209974e7874a1594f4533450d4da901
cho4u4o <cho4u4o@gmail.com>
2025-04-14 20:28:50 +0900

    feat(web): add endpoint of refresh

--- apps/web/src/shared/api/request.ts
+++ apps/web/src/shared/api/request.ts
@@ -1,6 +1,7 @@
 export const REQUEST = {
   LOGIN: '/login',
   SIGNUP: '/v1/user/join',
+  REFRESH: '/v1/user/refresh',
   CHECK_VALID_ID: '/v1/user/valid/id',
   CHECK_VALID_EMAIL_CODE: '/v1/email/auth',
   SEND_EMAIL_CODE: '/v1/email/auth/send',
