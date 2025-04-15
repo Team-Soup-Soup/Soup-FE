18335046107512fcdacd6c6901fecebc99d0933b
cho4u4o <cho4u4o@gmail.com>
2025-04-15 10:45:28 +0900

    feat(web): add `userPost` method with authorization

--- apps/web/src/shared/atoms/user.ts
+++ apps/web/src/shared/atoms/user.ts
@@ -18,30 +18,3 @@ export const userAtom = atomWithStorage<Token>(
   initialUserState,
   sessionStorage as AsyncStorage<Token>,
 );
-
-// const authAtom = atom<User | null>(null);
-
-// auth 상태를 갱신하는 atom (초기, 로그인, 로그아웃 시)
-// const initAuthAtom = atom(null, async (get, set) => {
-//   const token = {
-//     access: sessionStorage.getItem('accessToken'),
-//     refresh: localStorage.getItem('refreshToken'),
-//   };
-
-//   if (token.access) {
-//     const authResponse = await getMe();
-//     if (authResponse.data) return set(authAtom, authResponse.data);
-//   }
-
-//   if (token.refresh) {
-//     const refreshResponse = await refreshAccessToken();
-//     if (refreshResponse.accessToken) {
-//       sessionStorage.setItem('accessToken', refreshResponse.accessToken);
-
-//       const authResponse = await getMe();
-//       if (authResponse.data) return set(authAtom, authResponse.data);
-//     }
-//   }
-
-//   return set(authAtom, null);
-// });
