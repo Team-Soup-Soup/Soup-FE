ed43f687570f8beb1ce4f415e02eaa9a5b7fd401
cho4u4o <cho4u4o@gmail.com>
2025-04-16 02:19:01 +0900

    feat(web): get joined project data and connect with UI

--- apps/web/src/pages/project/ui/ProjectPage.tsx
+++ apps/web/src/pages/project/ui/ProjectPage.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+
+import { useQuery } from '@tanstack/react-query';
+import { useParams } from 'react-router-dom';
 import { ProjectContainer } from '~/widgets/project/ui';
+import { fetchProjectInfo } from '../api';
 
 export default function ProjectPage() {
+  const { projectId } = useParams();
+
+  const { data, isFetched } = useQuery({
+    queryKey: [`project${projectId}`],
+    queryFn: () => fetchProjectInfo(projectId!),
+  });
+
   return (
     <div className="h-full w-full p-8">
-      <ProjectContainer />
+      {isFetched && <ProjectContainer {...data!.data} />}
     </div>
   );
 }
--- apps/web/src/widgets/project/ui/ProjectContainer.tsx
+++ apps/web/src/widgets/project/ui/ProjectContainer.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Project } from '~/shared/types';
 import {
   GroupBoardSection,
   ProjectHeader,
   ProjectMenuContainer,
 } from '~/widgets/project/ui';
 
-export default function ProjectContainer() {
+export default function ProjectContainer({ description, name }: Project) {
   return (
     <div className="scrollbar-hide h-full overflow-scroll">
-      <ProjectHeader />
+      <ProjectHeader description={description} name={name} />
       <ProjectMenuContainer />
       <GroupBoardSection />
     </div>
