74df3a09c51b67c79941ff48b9075367ce6eea57
cho4u4o <cho4u4o@gmail.com>
2025-07-11 01:54:23 +0900

    feat(web): connect social login

--- apps/web/src/widgets/login/ui/SocialLogin.tsx
+++ apps/web/src/widgets/login/ui/SocialLogin.tsx
@@ -4,12 +4,18 @@ import Kakao from '~/assets/icons/kakao.svg';
 export default function SocialLogin() {
   return (
     <>
-      <div className="text-main-board-border mb-4 mt-16 flex items-center justify-between space-x-4 text-center font-light">
-        <hr className="border-main-board-border w-1/3" />
-        간편 로그인
-        <hr className="border-main-board-border w-1/3" />
+      <div className="text-light mb-4 mt-16 flex items-center justify-between space-x-4 text-center font-light">
+        <hr className="border-light w-1/3" />
+        간편 로그인&nbsp;&nbsp;&nbsp;&nbsp;
+        <hr className="border-light w-1/3" />
       </div>
-      <button className="bg-kakao hover:bg-kakao-dark text-dark auth-button px-7 py-4 font-light">
+      <button
+        className="bg-kakao hover:bg-kakao-dark text-dark auth-button px-7 py-4 font-light duration-300 ease-in-out"
+        onClick={() => {
+          window.location.href =
+            'http://student-p.p-e.kr/api/oauth2/authorization/kakao';
+        }}
+      >
         <img src={Kakao} className="mr-4 h-[20px] w-[20px]" />
         카카오톡 간편 로그인
       </button>
