e783d7d4c3fc58b129f4849c554336167c525215
cho4u4o <cho4u4o@gmail.com>
2025-07-16 01:56:46 +0800

    refactor : make `AuthRouter` to use path constant

--- apps/web/src/app/routers/AuthRouter.tsx
+++ apps/web/src/app/routers/AuthRouter.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
+import { PATH } from '~/shared/constants';
 import { fetchLoginStatus } from '~/shared/utils';
 
 export default function AuthRouter() {
-  return fetchLoginStatus() ? <Outlet /> : <Navigate to="/login" />;
+  return fetchLoginStatus() ? <Outlet /> : <Navigate to={PATH.LOGIN} />;
 }
